722b5860e69f6f22f00d1814a4abddd2536c7681
Maouloud-Fall <140567869+Maouloud-Fall@users.noreply.github.com>
2025-03-18 16:35:20 +0000

    second commit

--- src/app/components/playing-card/playing-card.component.ts
+++ src/app/components/playing-card/playing-card.component.ts
@@ -2,13 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-playing-card',
-  imports: [],
-  template: `
-    <p>
-      playing-card works!
-    </p>
-  `,
-  styles: ``
+  imports: [PlayingCardComponent],
+  templateUrl: `./app.component.html`,
+  styleUrls: ['./app.component.css'],
 })
 export class PlayingCardComponent {
 
